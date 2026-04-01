f80931ce853d77d06c673cce647a4764578b6cba
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-04-01 13:36:49 +0200

    Add empty widget.

---
+++ swiftui-github-activity-widget/swiftui_github_activity_widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  swiftui_github_activity_widgetBundle.swift
+//  swiftui-github-activity-widget
+//
+//  Created by Amadeusz Kosik on 01/04/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct swiftui_github_activity_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        swiftui_github_activity_widget()
+    }
+}
