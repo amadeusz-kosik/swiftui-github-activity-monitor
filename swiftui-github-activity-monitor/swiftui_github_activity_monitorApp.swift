c526844fac7d4cf10555e0a7ef7d65557704c1e6
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-04-01 13:29:43 +0200

    Initial Commit

---
+++ swiftui-github-activity-monitor/swiftui_github_activity_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_github_activity_monitorApp.swift
+//  swiftui-github-activity-monitor
+//
+//  Created by Amadeusz Kosik on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_github_activity_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
